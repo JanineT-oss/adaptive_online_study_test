d285bf5bd0ac1882ea8f131eecccc2fd65682c68
Janine Thome <59051486+JanineT-oss@users.noreply.github.com>
2022-02-23 10:04:25 +0100

    Add files via upload

--- 4_ajax/users.php
+++ 4_ajax/users.php
@@ -5,7 +5,7 @@ $conn = mysqli_connect('localhost', 'root', 'root', 'ajaxtest');
 $query= 'SELECT * FROM users';
 
 // Get results
-$result = mysqli_query($conn, $query);
+$results = mysqli_query($conn, $query);
 
 // Fetch data
 $users =mysqli_fetch_all($results, MYSQLI_ASSOC);
