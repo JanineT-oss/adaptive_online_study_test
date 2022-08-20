5279e9eee701a3c9f62d575180365db2a9fcb23e
Janine Thome <59051486+JanineT-oss@users.noreply.github.com>
2022-08-20 13:18:07 +0200

    Add files via upload

--- 1_adaptLearn_test/saveExp1.php
+++ 1_adaptLearn_test/saveExp1.php
@@ -7,9 +7,10 @@ echo $post_data;
 // the directory "data" must be writable by the www-data user!
 // path to directory
 $name = "data/test_exp1.csv";
+$name2 = "data/test_exp1.txt";
 
 
 // write the file to disk
 file_put_contents($name, json_encode($post_data));
-
+file_put_contents($name2, json_encode($post_data));
 ?>
